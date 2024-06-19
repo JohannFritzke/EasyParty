b74592528f8634d3ffbd05f95986a1d5131a073b
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2024-06-18 21:20:10 -0300

    Update LoginPage.tsx

--- Front/src/pages/LoginPage.tsx
+++ Front/src/pages/LoginPage.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
 import axios from 'axios';
 import logo from "../img/logoo-new-png.png";
 import { Instagram, Facebook, Twitter } from "lucide-react";
