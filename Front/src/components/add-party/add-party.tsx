ff8936140597dbf4aedbf51bbb2e09189dd73ed2
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2024-06-27 21:15:19 -0300

    AddParty
    Adicionado campos no AddParty

--- Front/src/components/add-party/add-party.tsx
+++ Front/src/components/add-party/add-party.tsx
@@ -1,6 +1,9 @@
 import { Plus } from "lucide-react";
 import "./add-party.css";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+
 export function AddParty() {
   return (
     <Dialog>
@@ -9,7 +12,34 @@ export function AddParty() {
           <Plus />
         </div>
       </DialogTrigger>
-      <DialogContent></DialogContent>
+      <DialogContent>
+        <h1>Adicionar um novo Evento!!</h1>
+
+        <form action="" className="flex justify-center flex-col">
+          <div className="flex w-[90%] justify-between p-3">
+            <div>
+              <p>Nome do Evento</p>
+              <Input placeholder="Nome do Evento" />
+            </div>
+            <div>
+              <p>Data do Evento</p>
+              <input
+                type="datetime-local"
+                className={cn(
+                  "uppercase input-date border-input disabled:opacity-50"
+                )}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Endereço</p>
+              <Input placeholder="Informe o Endereço"></Input>
+            </div>
+          </div>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
