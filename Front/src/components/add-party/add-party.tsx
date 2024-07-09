1d7d656d72a1b158672e5775addef87bb180efb7
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2024-07-09 20:45:48 -0300

    Event Alert
    Adicionado alertas estilizados

--- Front/src/components/add-party/add-party.tsx
+++ Front/src/components/add-party/add-party.tsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -6,14 +6,15 @@ import { cn } from "@/lib/utils";
 import axios from "axios";
 import est from "./estadosBrasil.json";
 import { useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+
 interface AddPartyProps {
   onClose(): void;
-  onAddEvent(): void;
+  onAddEvent(eventAdded: boolean): void;
 }
-export function AddParty({
-  onClose,
-  onAddEvent
-}: AddPartyProps) {
+
+export function AddParty({ onClose, onAddEvent }: AddPartyProps) {
   const [address, setAddress] = useState({
     cep: "",
     street: "",
@@ -23,8 +24,7 @@ export function AddParty({
   });
 
   const [number, setNumber] = useState("");
-  const [message, setMessage] = useState("");
-  const user=useParams<{ userName: string }>();
+  const user = useParams<{ userName: string }>();
 
   const handleCepChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const cep = e.target.value;
@@ -59,139 +59,135 @@ export function AddParty({
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     const formData = new FormData(e.currentTarget);
     const nomeDoEvento = formData.get("nomeDoEvento") as string;
     const dataDoEvento = formData.get("dataDoEvento") as string;
     const fullAddress = `${address.street}, ${address.city},${address.neighborhood}, ${address.state},Brasil`;
 
-    const response = await axios.get(
-      `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
-        fullAddress
-      )}`
-    );
-    console.log( `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
-      fullAddress
-    )}`)
+    try {
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+          fullAddress
+        )}`
+      );
 
-    if(response.data.length >0){
-      const eventInfo = {
-        nomeDoEvento,
-        dataDoEvento,
-        cep: address.cep,
-        street: address.street,
-        neighborhood: address.neighborhood,
-        city: address.city,
-        state: address.state,
-        number,
-        lat: response.data[0].lat,
-        lng: response.data[0].lon,
-        user:user.userName,
-      }; 
-      try {
-        await axios.post(
-          "http://localhost:3000/adicionar-evento",
-          eventInfo
-        );
-        onAddEvent();
+      if (response.data.length > 0) {
+        const eventInfo = {
+          nomeDoEvento,
+          dataDoEvento,
+          cep: address.cep,
+          street: address.street,
+          neighborhood: address.neighborhood,
+          city: address.city,
+          state: address.state,
+          number,
+          lat: response.data[0].lat,
+          lng: response.data[0].lon,
+          user: user.userName,
+        };
+
+        await axios.post("http://localhost:3000/adicionar-evento", eventInfo);
+        onAddEvent(true);
         onClose();
-        setMessage("Evento adicionado com sucesso!");
-      } catch (error) {
-        setMessage("Erro ao adicionar o evento.");
-        console.error("Erro ao salvar o evento:", error);
+        console.log("Evento adicionado com sucesso!");
+        toast.success("ksanmdskas")
+      } else {
+        toast.error("Local não mapeado!");
       }
-    }else{
-      alert("Local não mapeado!!")
+    } catch (error) {
+      toast.error("Erro ao adicionar o evento.");
     }
-    
   };
 
   return (
-    <Dialog open={true} onOpenChange={onClose}>
-      <DialogContent className="flex flex-col items-center w-[100rem]">
-        <h1>Adicionar um novo Evento</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col w-[85%]">
-          <div className="flex w-full justify-between">
-            <div>
-              <p>Nome do Evento</p>
-              <Input name="nomeDoEvento" placeholder="Nome do Evento" />
-            </div>
-            <div>
-              <p>Data do Evento</p>
-              <input
-                name="dataDoEvento"
-                type="datetime-local"
-                className={cn(
-                  "uppercase flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                )}
-              />
-            </div>
-          </div>
-
-          <div className="mt-2">
-            <div>
-              <p>CEP</p>
-              <Input
-                placeholder="Informe o CEP"
-                value={address.cep}
-                onChange={handleCepChange}
-              />
-            </div>
-            <div className="flex gap-1 mt-2">
+    <>
+      <Dialog open={true} onOpenChange={onClose}>
+        <DialogContent className="flex flex-col items-center w-[100rem]">
+          <h1>Adicionar um novo Evento</h1>
+          <form onSubmit={handleSubmit} className="flex flex-col w-[85%]">
+            <div className="flex w-full justify-between">
               <div>
-                <p>Bairro</p>
-                <Input
-                  placeholder="Bairro"
-                  value={address.neighborhood}
-                  readOnly
+                <p>Nome do Evento</p>
+                <Input name="nomeDoEvento" placeholder="Nome do Evento" />
+              </div>
+              <div>
+                <p>Data do Evento</p>
+                <input
+                  name="dataDoEvento"
+                  type="datetime-local"
+                  className={cn(
+                    "uppercase flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                  )}
                 />
               </div>
+            </div>
+
+            <div className="mt-2">
               <div>
-                <p>Cidade</p>
-                <Input placeholder="Cidade" value={address.city} readOnly />
+                <p>CEP</p>
+                <Input
+                  placeholder="Informe o CEP"
+                  value={address.cep}
+                  onChange={handleCepChange}
+                />
               </div>
-              <div>
-                <p>Estado</p>
-                <select
-                  value={address.state}
-                  onChange={handleStateChange}
-                  className="w-[100px] border rounded-md px-3 py-1 h-[36px] input"
-                >
-                  <option value="" disabled>
-                    Selecione
-                  </option>
-                  {est.estadosBrasil.map((estado: string) => (
-                    <option key={estado} value={estado}>
-                      {estado}
+              <div className="flex gap-1 mt-2">
+                <div>
+                  <p>Bairro</p>
+                  <Input
+                    placeholder="Bairro"
+                    value={address.neighborhood}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <p>Cidade</p>
+                  <Input placeholder="Cidade" value={address.city} readOnly />
+                </div>
+                <div>
+                  <p>Estado</p>
+                  <select
+                    value={address.state}
+                    onChange={handleStateChange}
+                    className="w-[100px] border rounded-md px-3 py-1 h-[36px] input"
+                  >
+                    <option value="" disabled>
+                      Selecione
                     </option>
-                  ))}
-                </select>
+                    {est.estadosBrasil.map((estado: string) => (
+                      <option key={estado} value={estado}>
+                        {estado}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="flex gap-6 mt-2">
+                <div>
+                  <p>Rua</p>
+                  <Input placeholder="Rua" value={address.street} readOnly />
+                </div>
+                <div>
+                  <p>Número</p>
+                  <Input
+                    name="numero"
+                    placeholder="Número"
+                    value={number}
+                    onChange={handleNumberChange}
+                  />
+                </div>
               </div>
             </div>
 
-            <div className="flex gap-6 mt-2">
-              <div>
-                <p>Rua</p>
-                <Input placeholder="Rua" value={address.street} readOnly />
-              </div>
-              <div>
-                <p>Número</p>
-                <Input
-                  name="numero"
-                  placeholder="Número"
-                  value={number}
-                  onChange={handleNumberChange}
-                />
-              </div>
-            </div>
-          </div>
-
-          <Button type="submit" className="mt-3 bg-easy text-white ">
-            Salvar
-          </Button>
-        </form>
-        {message && <p>{message}</p>}
-      </DialogContent>
-    </Dialog>
+            <Button type="submit" className="mt-3 bg-easy text-white ">
+              Salvar
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <ToastContainer className="z-50" autoClose={3000}/>
+    </>
   );
 }
--- Front/src/pages/Home.tsx
+++ Front/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
 import { ThemeProvider } from "../components/theme-provider";
 import { ModeToggle } from "../components/mode-toggle";
 import { Sidebar } from "../components/Sidebar/side-bar";
@@ -9,8 +8,9 @@ import { Search } from "../components/search/search";
 import { AddParty } from "../components/add-party/add-party";
 import iconUrl from "../img/1.ico";
 import axios from "axios";
-import { format } from 'date-fns';
-
+import { format } from "date-fns";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
 interface EventInfo {
   nomeDoEvento: string;
@@ -26,11 +26,10 @@ interface EventInfo {
 }
 
 export function Home() {
-  const mapRef = useRef<L.Map | any>(null); // Ref inicializado com null
+  const mapRef = useRef<L.Map | any>(null);
   const [showAddParty, setShowAddParty] = useState(false);
   const [markerPosition, setMarkerPosition] = useState<L.LatLng | null>(null);
-  const [markers, setMarkers] = useState<{ marker: L.Marker; info: any }[]>([]);
-  
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
   const customIcon = new L.Icon({
     iconUrl: iconUrl,
     iconSize: [32, 32],
@@ -55,20 +54,24 @@ export function Home() {
         setShowAddParty(true);
       });
 
-      mapRef.current = mymap; // Atribuição de mymap a mapRef.current
+      mapRef.current = mymap;
     }
   }, []);
+
   const fetchEvents = async () => {
     try {
       const response = await axios.get("http://localhost:3000/get-events");
-      const events: EventInfo[] = response.data; // Tipo explícito para events
+      const events: EventInfo[] = response.data;
 
       if (mapRef.current) {
         events.forEach((event: EventInfo) => {
           const marker = L.marker([event.lat, event.lng], {
             icon: customIcon,
           });
-          const formattedDate = format(new Date(event.dataDoEvento), 'dd/MM/yyyy HH:mm');
+          const formattedDate = format(
+            new Date(event.dataDoEvento),
+            "dd/MM/yyyy HH:mm"
+          );
           const popupContent = `
             <div>
               <h3>${event.nomeDoEvento}</h3>
@@ -82,28 +85,14 @@ export function Home() {
             </div>
           `;
           marker.bindPopup(popupContent);
-
-          marker.on("contextmenu", () => {
-            if (mapRef.current) {
-              mapRef.current.removeLayer(marker);
-              setMarkers((prevMarkers) =>
-                prevMarkers.filter((m) => m.marker !== marker)
-              );
-            }
-          });
-
-          marker.addTo(mapRef.current); // Adicionar o marcador ao mapa
-
-          setMarkers((prevMarkers) => [
-            ...prevMarkers,
-            { marker, info: event },
-          ]);
+          marker.addTo(mapRef.current);
         });
       }
     } catch (error) {
       console.error("Erro ao buscar eventos:", error);
     }
   };
+
   useEffect(() => {
     fetchEvents();
   }, []);
@@ -112,7 +101,20 @@ export function Home() {
     setShowAddParty(false);
     setMarkerPosition(null);
   };
-  
+
+  const handleAddEvent = async (eventAdded: boolean) => {
+    if (eventAdded) {
+      await fetchEvents();
+      setShowSuccessToast(true);
+      setTimeout(() => {
+        toast.success("Evento adicionado com sucesso!");
+      }, 100);
+      setTimeout(() => {
+        setShowSuccessToast(false);
+      }, 4000); // Aguarda um pequeno atraso para a atualização do estado ser refletida
+    }
+  };
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex absolute w-full">
@@ -120,10 +122,12 @@ export function Home() {
         <Sidebar />
         <Search />
         {showAddParty && markerPosition && (
-          <AddParty onClose={handleClose} onAddEvent={()=>fetchEvents()} />
+          <AddParty onClose={handleClose} onAddEvent={handleAddEvent} />
         )}
       </div>
       <div id="map" style={{ height: "100vh" }} className="z-0"></div>
+
+      {showSuccessToast && <ToastContainer autoClose={3000} className="z-50" />}
     </ThemeProvider>
   );
 }
